7d9ea0aa0a56c1a6e025d6c05a2b83a2b10494dd
강진우 <wlsdn1252@naver.com>
2023-01-18 23:18:56 +0900

    리스트뷰 화면 구현

--- app/src/main/java/com/example/listviewpractice3/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractice3/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.listviewpractice3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listviewpractice3.adapters.Adapter
+import com.example.listviewpractice3.datas.PhoneData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdapter : Adapter
+    val phoneList = ArrayList<PhoneData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        phoneList.add(PhoneData("강진우","갤럭시s22+"))
+        phoneList.add(PhoneData("이봉준","아이폰+"))
+        phoneList.add(PhoneData("ㅁㅁㅁ","샤오미"))
+        phoneList.add(PhoneData("ㅇㅇㅇ","ㅏㅏㅏ"))
+        phoneList.add(PhoneData("ㄱㄱㄱ","ㅂㅂㅂ"))
+        phoneList.add(PhoneData("ㅅㅅㅅ","ㅇㄻㅈㄷ"))
+
+        mAdapter = Adapter(this,R.layout.phone_infomation_item_list,phoneList)
+
+        mainListView.adapter = mAdapter
+
+
+
     }
 }
